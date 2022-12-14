2e1c0a4a43f216cf927e87276e61479d03d4c2d8
Jessica Petrey <jesipetrey@gmail.com>
2022-12-14 09:17:43 -0800

    inital commit

---
+++ Algorithms/script.js
@@ -0,0 +1,11 @@
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+for(var i = 0; i < numbers.length; i++)
+    if(numbers[i] > 0) {
+        countPositives ++
+    }
+    
+console.log("there are " + countPositives + " positive values");
+
+
+// not enough time working with others to do it together, this is my solo solution
