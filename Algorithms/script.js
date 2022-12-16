b7d8039c6c2202d71b4a39b26b0a6380d4f1ae0e
Jessica Petrey <jesipetrey@gmail.com>
2022-12-16 12:27:29 -0800

    initial commit, fizzbuzz

--- Algorithms/script.js
+++ Algorithms/script.js
@@ -9,3 +9,18 @@ console.log("there are " + countPositives + " positive values");
 
 
 // not enough time working with others to do it together, this is my solo solution
+
+
+
+
+
+var arr = ["a", "b", "c", "d", "e"];
+function reverse(arr){
+    for(i=1; i<arr.length/2 - 1; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length - 1 -i];
+        arr[arr.length - 1] = temp;
+        return 
+    }
+}
+console.log(reverse)
