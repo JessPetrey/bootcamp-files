498860e85f25f356633d3aedf94052cf3dfd6e0f
Jessica Petrey <jesipetrey@gmail.com>
2022-12-16 14:31:37 -0800

    yes

---
+++ JS/loop_challenges/script.js
@@ -0,0 +1,28 @@
+// print odds 1-20
+var odd = 0
+
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 == 1) {
+        odd = i;
+        console.log(odd)
+    }
+}
+
+// decreasing multiples of 3
+var num = 100
+
+for (var i = num; i >= 0; i--) {
+    if (i % 3 == 0) {
+        num = i;
+        console.log(num)
+    }
+}
+
+// print the sequence
+var sequence = [4, 2.5, 1, -0.5, -2, -3.5] 
+    for ()
+
+
+// sigma
+
+// factorial
