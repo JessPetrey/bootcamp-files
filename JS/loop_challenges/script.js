cf6dae07474695825d5e1c79cd701a8bfbef28d8
Jessica Petrey <jesipetrey@gmail.com>
2022-12-16 16:09:46 -0800

    adjustment

--- JS/loop_challenges/script.js
+++ JS/loop_challenges/script.js
@@ -1,6 +1,5 @@
 // print odds 1-20
 var odd = 0
-
 for (var i = 0; i <= 20; i++) {
     if (i % 2 == 1) {
         odd = i;
@@ -9,18 +8,15 @@ for (var i = 0; i <= 20; i++) {
 }
 
 // decreasing multiples of 3
-var num = 100
-for (var i = num; i > 0; i--) {
+for (var i = 100; i > 0; i--) {
     if (i % 3 == 0) {
-        num = i;
         console.log(num)
     }
 }
 
 // print the sequence
-var sequence = [4, 2.5, 1, -0.5, -2, -3.5]
-    for(var i = 0; i < sequence.length; i++) {
-        console.log(sequence[i])
+    for(var i = 4; i > -4; i -= 1.5) {
+        console.log(i)
     }
 
 // sigma
