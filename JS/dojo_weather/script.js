38161eb2c98e2aa3b567be66b449d0fcb11f7f91
Jessica Petrey <jesipetrey@gmail.com>
2022-12-20 14:57:22 -0800

    final commit for now, dojoweather

--- JS/dojo_weather/script.js
+++ JS/dojo_weather/script.js
@@ -1,19 +1,39 @@
 // Celsius to Fahrenheit Formula: (°C * 1.8) + 32 = °F
-
-
 // Fahrenheit to Celsius Formula: (°F - 32) / 1.8 = °C
 
-function convertTemp() {
+// might come back to this, would change variables to the days
+// arrays with 2 values, high and low
+// could then make day functions to swap the innerText? 
+// this logic is tough rn...
 
+var highC = [24, 27, 21, 26];
+var lowC = [18, 19, 16, 21];
+function celsiusToF(high, low){
+    for (var i = 0; i < highC.length; i++) {
+        var highF = []
+        highF.push(Math.floor((highC[i] * 1.8) + 32))
+        console.log(highF)
+    }
+    for (var i = 0; i < lowC.length; i++) {
+        var lowF = []
+        lowF.push(Math.floor((lowC[i] * 1.8) + 32))
+        console.log(lowF)
+    }
+    return 
 }
+celsiusToF()
 
 
+// this works, only for "today"
 
+// function convertTemp() {
+//     var high = document.querySelector(".high")
+//     var low = document.querySelector(".low")
+//     high.innerText = ("75°")
+//     low.innerText = ("64°")
+// }
 
-
-
-
-    function dismissCookies() {
+function dismissCookies() {
     var dismiss = document.getElementById("dismiss");
     dismiss.remove();
 }
