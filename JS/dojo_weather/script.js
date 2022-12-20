6d1f44a01528b4f05fc891201b7a4fc52bfecc47
Jessica Petrey <jesipetrey@gmail.com>
2022-12-20 11:58:11 -0800

    added algos

---
+++ JS/dojo_weather/script.js
@@ -0,0 +1,20 @@
+// Celsius to Fahrenheit Formula: (°C * 1.8) + 32 = °F
+
+
+// Fahrenheit to Celsius Formula: (°F - 32) / 1.8 = °C
+
+function convertTemp() {
+
+}
+
+
+
+
+
+
+
+    function dismissCookies() {
+    var dismiss = document.getElementById("dismiss");
+    dismiss.remove();
+}
+
